c0b9358b712c67d796aabf5675e2ff2ee8951832
Bailey Kasin <bailey@gingertechnology.net>
2018-06-12 00:06:43 -0700

    I think the site is functional

--- ScoringEngine/opt/site/Controllers/HomeController.cs
+++ ScoringEngine/opt/site/Controllers/HomeController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using site.Models;
+
+using Newtonsoft.Json;
 
 namespace site.Controllers
 {
@@ -26,6 +29,17 @@ namespace site.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public JsonResult Checks()
+        {
+            using (StreamReader r = new StreamReader("wwwroot/js/current.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Check> checks = JsonConvert.DeserializeObject<List<Check>>(json);
+                //return list as Json
+                return Json(checks);
+            }
         }
     }
 }
